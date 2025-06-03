78fa30391cc32e6fe91d85bbeafa038dbd653cf1
Juan Terneus <juanjose.terneus@verndale.com>
2025-06-03 14:50:28 -0500

    adding optimized images

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false, // Keep original files
+      threshold: 10240, // Only compress files larger than 10kb
+      compressionOptions: {
+        level: 11, // Maximum compression level
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
